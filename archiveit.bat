26531901315a01d8250ae608074f6489c571f0c4
Johnny Appleseed <jn.apsd@gmail.com>
2021-05-20 13:25:12 +0800

    v1.0.3 - fix the only-one-argument bug
    If there is only one argument, the script will fail to realize that user
    didn't provide the archive-directory.

--- archiveit.bat
+++ archiveit.bat
@@ -1,5 +1,5 @@
 @setlocal
-@set "version=v1.0.2"
+@set "version=v1.0.3"
 @set "lupdate=2021-05-20"
 
 @if /i "%~1" == "/?" goto help
@@ -74,16 +74,16 @@
     )
 )
 
+@shift
+@goto paramparse
+
+:postparamparse
+
 @if not defined archive_dir (
     @>&2 echo ARCHIVEIT: ERROR: archive directory is not specified.
     @>&2 call:Help
     exit /b 9
 )
-
-@shift
-@goto paramparse
-    
-:postparamparse
 
 @if not exist "%archive_dir%\%target_filename%.7z" goto continue_already_existed
 @if defined overwrite goto continue_already_existed
